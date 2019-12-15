9d69c910873372815a4633754391325c40d719b1
yrong10 <48108058+yrong10@users.noreply.github.com>
2019-12-15 00:01:39 -0500

    Add files via upload

---
+++ raspberrypi/Stream/camera.py
@@ -0,0 +1,62 @@
+import cv2
+import time
+
+pre_frame = None
+motion_time = 0
+
+class VideoCamera():
+
+    def __init__(self):
+        # Open a camera
+        self.camera = cv2.VideoCapture(0)
+
+    def get_frame(self):
+        fps = 24 #set frames per second
+        global pre_frame
+        global motion_time
+        while True:
+            start = time.time()
+            res, cur_frame = self.camera.read()
+            if res != True:
+                break
+            end = time.time()
+            seconds = end - start
+            # make sure each second has correct no. of frames
+            if seconds < 1.0/fps:
+                time.sleep(1.0/fps - seconds)
+
+            # RGB to Gray
+            gray_img = cv2.cvtColor(cur_frame, cv2.COLOR_BGR2GRAY)
+            gray_img = cv2.resize(gray_img, (500, 500))
+            gray_img = cv2.GaussianBlur(gray_img, (21, 21), 0)
+
+            # motion detection
+            if pre_frame is None:
+                pre_frame = gray_img
+            else:
+                img_delta = cv2.absdiff(pre_frame, gray_img)
+                thresh = cv2.threshold(img_delta, 25, 255, cv2.THRESH_BINARY)[1]
+                thresh = cv2.dilate(thresh, None, iterations=2)
+                contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                for c in contours:
+                    if cv2.contourArea(c) < 1500: # sensitivity
+                        continue
+                    else:
+                        # set cooldown time to 2 sec
+                        if time.time() - motion_time < 2:
+                            continue
+                        else:
+                            # print when motion detected
+                            # add output here if needed
+                            print("Motion detected")
+                            motion_time = time.time()
+                            break
+
+                pre_frame = gray_img
+
+            # prepare frame for stream
+            if res:
+                res, jpeg = cv2.imencode('.jpg', cur_frame)
+                return jpeg.tobytes()
+            else:
+                return None
--- raspberrypi/Stream/stream.py
+++ raspberrypi/Stream/stream.py
@@ -1,26 +1,34 @@
 from flask import Flask, render_template, Response
-import cv2
-import sys
-import numpy
+from camera import VideoCamera
 
 app = Flask(__name__)
+
+first_frame=None
+video_camera = None
+global_frame = None
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def get_frame():
-    camera_port=0
-    camera=cv2.VideoCapture(camera_port) #this makes a web cam object
+    global video_camera
+    global global_frame
+
+    if video_camera == None:
+        video_camera = VideoCamera()
 
     while True:
-        retval, im = camera.read()
-        imgencode=cv2.imencode('.jpg',im)[1]
-        stringData=imgencode.tostring()
-        yield (b'--frame\r\n'
-            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
+        frame = video_camera.get_frame()
 
-    del(camera)
+        if frame != None:
+            global_frame = frame
+            yield (b'--frame\r\n'
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            yield (b'--frame\r\n'
+                            b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
+
 
 @app.route('/calc')
 def calc():
