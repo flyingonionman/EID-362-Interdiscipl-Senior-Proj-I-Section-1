821fbbf1466ff23222da8d986adf704a588395e2
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2019-12-16 17:02:34 -0500

    message receiving works on phone
    now to send messages...

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { Image, StyleSheet, View,Alert,BackHandler} from "react-native";
+import { Image, StyleSheet, View,Alert,BackHandler,AsyncStorage } from "react-native";
 import WebView from "react-native-webview";
 import Button from "./Button";
 import colors from "../config/colors";
 import strings from "../config/strings";
+import firebase from 'react-native-firebase';
 
 
 interface State {
@@ -23,13 +24,91 @@ class Home extends React.Component<{}, State> {
     readonly state: State = {
         email: ""
       };
+
       
+    async checkPermission() {
+      const enabled = await firebase.messaging().hasPermission();
+      if (enabled) {
+        console.log('ooga token');
+          this.getToken();
+      } else {
+          this.requestPermission();
+      }
+    }
+
+    async getToken() {
+      let fcmToken = await AsyncStorage.getItem('fcmToken');
+      console.log(fcmToken);
+
+      if (!fcmToken) {
+          fcmToken = await firebase.messaging().getToken();
+          if (fcmToken) {
+              // user has a device token
+              console.log('got token');
+              await AsyncStorage.setItem('fcmToken', fcmToken);
+          }
+      }
+    }
+    
+    async requestPermission() {
+      try {
+          await firebase.messaging().requestPermission();
+          console.log('ooga rejected');
+          this.getToken();
+      } catch (error) {
+          // User has rejected permissions
+          console.log('permission rejected');
+      }
+    }
+
     componentDidMount() {
-      BackHandler.addEventListener('hardwareBackPress', this.backPress)
+      BackHandler.addEventListener('hardwareBackPress', this.backPress);
+      this.checkPermission();
+      this.createNotificationListeners(); //add this line
+
     }
 
     componentWillUnmount() {
-      BackHandler.removeEventListener('hardwareBackPress', this.backPress)
+      BackHandler.removeEventListener('hardwareBackPress', this.backPress);
+      this.notificationListener();
+      this.notificationOpenedListener();
+    }
+
+    async createNotificationListeners() {
+      /*
+      * Triggered when a particular notification has been received in foreground
+      * */
+      console.log("lol I'm fucked")
+
+      this.notificationListener = firebase.notifications().onNotification((notification) => {
+
+          const { title, body } = notification;
+          this.showAlert(title, body);
+      });
+    
+      /*
+      * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+      * */
+      this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+          const { title, body } = notificationOpen.notification;
+          this.showAlert(title, body);
+      });
+    
+      /*
+      * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+      * */
+      const notificationOpen = await firebase.notifications().getInitialNotification();
+      if (notificationOpen) {
+          const { title, body } = notificationOpen.notification;
+          this.showAlert(title, body);
+      }
+      /*
+      * Triggered for data only payload in foreground
+      * */
+      this.messageListener = firebase.messaging().onMessage((message) => {
+        //process data message
+        console.log(JSON.stringify(message));
+      });
     }
 
     backPress = () => true
@@ -65,6 +144,16 @@ class Home extends React.Component<{}, State> {
 
     };
   
+    showAlert(title, body) {
+      console.log('Kill me')
+      Alert.alert(
+        title, body,
+        [
+            { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false },
+      );
+    }
   render() { 
     const {
         email
@@ -74,7 +163,7 @@ class Home extends React.Component<{}, State> {
       <View style={styles.container}>
 
         <View style={styles.webview}>
-          <WebView source={{ uri: 'https://facebook.github.io/react-native/' }} />
+          <WebView source={{ uri: 'https://google.com' }} />
         </View>
         <View style={styles.buttons}>
           <Button
