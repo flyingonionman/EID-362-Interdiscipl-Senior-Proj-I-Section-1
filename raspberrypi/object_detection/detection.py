56c26a493da09572e248fabb5d37c71ad8628aed
yrong10 <48108058+yrong10@users.noreply.github.com>
2020-02-24 19:04:41 -0500

    Update detection.py

--- raspberrypi/object_detection/detection.py
+++ raspberrypi/object_detection/detection.py
@@ -1,13 +1,6 @@
 # USAGE
-# To read and write back out to video:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
-#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 \
-#	--output output/output_01.avi
-#
-# To read from webcam and write back out to disk:
-# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
-#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
-#	--output output/webcam_output.avi
+# python3 detection.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel
+
 
 # import the necessary packages
 from person.centroidtracker import CentroidTracker
